b53ccd2065abfbfdc0fb6a2321620c506fb924ed
“Shankar” <shankarsaravanan40@gail.com>
2022-05-24 20:45:54 +0530

    v1

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,9 +25,10 @@ const Layout = ({ location, title, children }) => {
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}, Built with
+        Built using <a href="https://www.gatsbyjs.com/">Gatsby</a>
+        {/* | ©{" "}
+        {new Date().getFullYear()}, Shankar Saravanan */}
         {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
       </footer>
     </div>
   )
