44dfdec8c587d8429ae247e33a13afc68378868d
“Shankar” <shankarsaravanan40@gail.com>
2022-05-24 20:16:21 +0530

    v0.2

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Shankar Saravanan`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: ``,
+      summary: `Hi, I'm Shankar. I'm Currently working as a Digital Product Designer.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
